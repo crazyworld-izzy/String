f7c3b0c45367f325a78574dae7198b7bfc92ec9f
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-11-11 22:24:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "24818964"))
+API_HASH = getenv("API_HASH", "1ab692fe8efac1343c7d6a6ee1d4194f")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "8120562276:AAHVyD_2E8J45GdsdlQkg3hCUhfWks1sxoY")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://marwin2002:7Yw81GpqZq2a9j4R@cluster0.rgqgp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+OWNER_ID = int(getenv("OWNER_ID", 7078122796))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/izzy_tamil_junction")
